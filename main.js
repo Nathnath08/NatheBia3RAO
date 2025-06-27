a09a26827a9464854a83978f9eee0bfe7c134d50
Nathaliabalanote <160140114+Nathnath08@users.noreply.github.com>
2025-06-27 09:43:35 -0300

    nathalia

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-let randomNumber =
-Math. floor(Math.random()*9)+1;
-let attempts = 0;
+const botoes = document.querySelectorAll(".botao");
 
-function checkGuess = parseInt(document.getElementById('guess').value);
-attempts++;
+for (let i = 0; i < botoes.length; i++) {
+  botoes[i].onclick = function () {
+    for (let j = 0; j < botoes.length; j++) {
+      botoes[j].classList.remove("ativo");
+    }
 
-if(userGuess === )
+    botoes[i].classList.add("ativo");
+  };
+}
